8c84bfa18926ca8411b27d09635ea0c29f5efc24
Siwaphon Panisen <siwaphon08@gmail.com>
2025-08-13 19:48:40 +0700

    Add modules

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
-    <h1>Hello World</h1>
+  <div class="font-Montserrat container">
+    <h1 class="text-4xl font-bold text-blue-500 p-4">Hello World</h1>
   </div>
 </template>
